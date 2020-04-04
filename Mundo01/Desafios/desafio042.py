71708aff86bddecff4dcc96c14ba0a9d56e3b142
AlexsandroMatias2017 <matiasalexsandro@gmail.com>
2020-04-03 22:00:08 -0300

    Organizando melhor os diretórios

--- Mundo01/Desafios/desafio042.py
+++
@@ -1,3 +0,0 @@
-# Desafio 042
-# Refaça o desafio 35 dos triângulos,
-# acrescentando o recurso de mostrar que tipo de triângulo será formado.
--- Mundo01/Desafios/desafio045.py
+++
@@ -1,3 +0,0 @@
-#
-# Desafio 045
-# Crie um programa que faça o jogo JokePô -> PEDRA X PAPEL X TESOURA
