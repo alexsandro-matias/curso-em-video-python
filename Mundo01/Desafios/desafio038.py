68d49163232de31a9c60a25a1cda84460499c9c1
AlexsandroMatias2017 <matiasalexsandro@gmail.com>
2020-04-03 21:50:26 -0300

    Desafios 38, 39 e 40

--- Mundo01/Desafios/desafio038.py
+++ Mundo01/Desafios/desafio038.py
@@ -5,4 +5,14 @@
 # - Não existe valor maior, os dois valores são iguais.
 
 
+primeiro  = int(input('Digite o primeiro número: '))
+segundo = int(input('Digite o segundo número: '))
 
+if primeiro >  segundo:
+    print('O primeiro valor é maior')
+
+elif segundo > primeiro:
+    print('O segundo valor é maior')
+
+else:
+    print('Não existe valor maior, os dois valores são iguais.')
--- Mundo01/Desafios/desafio040.py
+++ Mundo01/Desafios/desafio040.py
@@ -1,5 +1,7 @@
 # Desafio 040
-# Crie um programa que leia duas notas de um aluno e calcule sua média, mostrando uma mensagem no final, de acordo com média atingida:
+# Crie um programa que leia duas notas de um aluno e calcule sua média,
+# mostrando uma mensagem no final, de acordo com média atingida:
 # - Média abaixo de 5,0 = REPROVADO
 # - Média entre 5,0 e 6,9 = RECUPERAÇÃO
 # - Média 7,0 ou superior = APROVADO
+
