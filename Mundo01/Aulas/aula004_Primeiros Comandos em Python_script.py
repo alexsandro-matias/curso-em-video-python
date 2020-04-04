2dcf33d6ea19db05700dfba55eef596d97a40cc6
AlexsandroMatias2017 <matiasalexsandro@gmail.com>
2020-04-04 20:27:30 -0300

    aula008_Utilizando_Modulos

--- Mundo01/Aulas/aula004_Primeiros Comandos em Python_script.py
+++ Mundo01/Aulas/aula004_Primeiros Comandos em Python_script.py
@@ -2,4 +2,4 @@ nome = input('Qual é a seu nome? ')
 idade = input('Qual é a sua idade? ')
 peso = input('Qual é o seu peso? ')
 
-print("Nome: "nome, "Idade: " idade, peso)
+# print("Nome: "nome, "Idade: " idade, peso)
--- Mundo01/Aulas/aula004_Primeiros Comandos em Python_teste.py
+++ Mundo01/Aulas/aula004_Primeiros Comandos em Python_teste.py
@@ -1,24 +1,24 @@
-Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> 
- RESTART: C:/Users/josiane/Documents/Alexsandro/CursoEmVideo/Python/Mundo01/Aula04/aula004_Primeiros Comandos em Python_script.py 
-Qual é a sua idade? 34
-Qual é a sua idade? 
- RESTART: C:/Users/josiane/Documents/Alexsandro/CursoEmVideo/Python/Mundo01/Aula04/aula004_Primeiros Comandos em Python_script.py 
-Qual é a seu nome? alexsandro
-Qual é a sua idade? 34
-Qual é o seu peso? 82.1
-alexsandro 34 82.1
->>> print('Olá '+nome 'prazer em ')
-Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
-SyntaxError: invalid syntax
->>> 
->>> 
- RESTART: C:/Users/josiane/Documents/Alexsandro/CursoEmVideo/Python/Mundo01/Aula04/desafio_01.py 
-Qual é a seu nome? alexsandro
-Olá alexsandro Prazer em te conhecer!
->>> 
- RESTART: C:/Users/josiane/Documents/Alexsandro/CursoEmVideo/Python/Mundo01/Aula04/desafio_01.py 
-Qual é a seu nome? matias
-Olá matias ! Prazer em te conhecer!
->>> 
+# Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+# Type "copyright", "credits" or "license()" for more information.
+# >>>
+#  RESTART: C:/Users/josiane/Documents/Alexsandro/CursoEmVideo/Python/Mundo01/Aula04/aula004_Primeiros Comandos em Python_script.py
+# Qual é a sua idade? 34
+# Qual é a sua idade?
+#  RESTART: C:/Users/josiane/Documents/Alexsandro/CursoEmVideo/Python/Mundo01/Aula04/aula004_Primeiros Comandos em Python_script.py
+# Qual é a seu nome? alexsandro
+# Qual é a sua idade? 34
+# Qual é o seu peso? 82.1
+# alexsandro 34 82.1
+# >>> print('Olá '+nome 'prazer em ')
+# Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+# SyntaxError: invalid syntax
+# >>>
+# >>>
+#  RESTART: C:/Users/josiane/Documents/Alexsandro/CursoEmVideo/Python/Mundo01/Aula04/desafio_01.py
+# Qual é a seu nome? alexsandro
+# Olá alexsandro Prazer em te conhecer!
+# >>>
+#  RESTART: C:/Users/josiane/Documents/Alexsandro/CursoEmVideo/Python/Mundo01/Aula04/desafio_01.py
+# Qual é a seu nome? matias
+# Olá matias ! Prazer em te conhecer!
+# >>>
