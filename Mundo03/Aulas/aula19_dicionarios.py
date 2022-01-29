56381bfc34a6897aff1aaf2af90b89acb41f691d
Alexsandro Matias de Almeida <matiasalexsandro@gmail.com>
2022-01-28 21:11:35 -0300

    Indo para os exercícios de dicionários

--- Mundo03/Aulas/aula19_dicionarios.py
+++ Mundo03/Aulas/aula19_dicionarios.py
@@ -33,39 +33,66 @@ filme = {
     'diretor': 'Wachowski',
 }
 
-
-print(filme)
-
+# print(locadora)
 # filme
 # star wars       1977        George Lucas (valores)
 # titulo          ano         diretor (chaves)
 
-
-# # Retornando os valores do dicionário, ou seja, o conteúdo das chaves
+#
+# # # Retornando os valores do dicionário, ou seja, o conteúdo das chaves
 # print(filme.values())
-#
-# # Retornando as chaves do dicionário:
+# #
+# # # Retornando as chaves do dicionário:
 # print(filme.keys())
-#
-# # Retornando ambos os valores:
+# #
+# # # Retornando ambos os valores:
 # print(filme.items())
 
-#
+
 # for chave, valor in filme.items():
 #     print(f'{chave} - {valor}')
 
-# print(filme[1])
+#
+# for k,v in enumerate(filme.items()):
+#     print(f'O {k} é {v}')
 
-# locadora = []
+
+# print(filme['ano'])
+
+
+# estado1 = {'uf': 'Rio de janeiro' , 'sigla': 'RJ'}
+# estado2 = {'uf': 'São Paulo' , 'sigla': 'SP'}
+# brasil = []
+# brasil.append(estado1)
+# brasil.append(estado2)
 #
-# locadora.append(filme[0])
-# #
-# # print(locadora)
+# print(brasil[1])
+
+# No cliclo for é necessária fazer a cópia dos valores, uma vez que:
+
+estado = dict()
+brasil = list()
+
+# for c in range(0,2):
+#     estado['uf'] = input('Unidade Federativa: ')
+#     estado['sigla'] = input('Sigla do estado: ')
+#     brasil.append(estado)
 #
-#
-# # for k,v in enumerate(filme.items()):
-# #     print(f'O {k} é {v}')
-#
-#
-# for i in filme:
-#     print(locadora[1]['ano'])
+# print(brasil)
+
+# Unidade Federativa: rio
+# Sigla do estado: rj
+# Unidade Federativa: sao paulo
+# Sigla do estado: sp
+# [{'uf': 'sao paulo', 'sigla': 'sp'}, {'uf': 'sao pau
+
+
+# Para evitar esse "erro",
+# existe um método nativo do python para fazer a cópia do dicionário .copy
+
+for c in range(0,2):
+    estado['uf'] = input('Unidade Federativa: ')
+    estado['sigla'] = input('Sigla do estado: ')
+    brasil.append(estado.copy())
+
+print(brasil)
