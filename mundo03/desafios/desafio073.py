9cf2f8c9e13ab8134b11899017af556d958dd312
alexsandro-matias <matiasalexsandro@gmail.com>
2024-03-01 21:21:27 -0300

    aula de tuplas e verificando os desafios de tuplas

---
+++ mundo03/desafios/desafio073.py
@@ -0,0 +1,8 @@
+# desafio 073
+# Crie uma tupla preenchida com os 20 primeiros colocados da tabela do campeonato brasileiro de futebol,
+# Na ordem de colocação. Depois mostre:
+# a) Apenas os 5 primeiros colocados.
+# b) Os últimos 4 colocados da tabela.
+# c) Uma lista com os times em ordem alfabética.
+# d) Em que posição na tabela está o time da Chapecoense.
+
---
+++ mundo03/desafios/desafio076.py
@@ -0,0 +1,9 @@
+
+# desafio 076
+# Crie um programa que tenha uma tupla única com nomes de produtos
+# e seus respectivos preços, na sequência
+#
+# No final, mostre uma listagem de preços organizando os dados em forma tabular.
+#
+# listagem = ('Pão',1, 'Leite', 3.50, 'Frango', 10.90).
+#
