f2fbd6d3195b802aac1bfba2afa44ef6ab9be6af
alexsandro-matias <matiasalexsandro@gmail.com>
2026-09-21 20:58:58 -0300

    Desafio 16

--- mundo04/desafios/desafio16.py
+++ mundo04/desafios/desafio16.py
@@ -1,1 +1,16 @@
-# Crie a classe Funcionario, onde podemos cadastrar nome, setor e cargo. Crie também um método que permita ao funcionário se apresentar.
+# Crie a classe Funcionario, onde podemos cadastrar nome, setor e cargo. Crie também um método que permita ao
+#  funcionário se apresentar.
+
+class Funcionario():
+    def __init__(self, nome, setor, cargo):
+        self.nome = nome
+        self.setor = setor
+        self.cargo = cargo
+
+    def __str__(self):
+        return f'Sou {self.nome} do {self.cargo} do setor {self.setor}'
+
+
+paula = Funcionario('paula', 'Analista de RH', 'Recursos Humanos')
+
+print(paula)
